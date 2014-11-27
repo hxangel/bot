845c29df848c311a4d960b8cf3b2a3012ebd258a
rainkid <rainkide@gmail.com>
2014-11-27 19:49:34 +0800

    update

--- routers.go
+++ routers.go
@@ -2,22 +2,14 @@ package main
 
 import (
 	"github.com/rainkid/dogo"
-	admin "modules/admin"
 	api "modules/api"
 	debug "modules/debug"
-	front "modules/front"
 )
 
 func AddSampleRoute(router *dogo.Router) {
-	router.AddSampleRoute("admin", &admin.Errors{})
-	router.AddSampleRoute("admin", &admin.Group{})
-	router.AddSampleRoute("admin", &admin.Index{})
-	router.AddSampleRoute("admin", &admin.Login{})
-	router.AddSampleRoute("admin", &admin.User{})
 	router.AddSampleRoute("api", &api.Mmb{})
 	router.AddSampleRoute("api", &api.Other{})
 	router.AddSampleRoute("api", &api.Taobao{})
 	router.AddSampleRoute("api", &api.Tmall{})
 	router.AddSampleRoute("debug", &debug.Pprof{})
-	router.AddSampleRoute("front", &front.Index{})
 }
